7bcdb28129b41cea45d5eac96c765c135a6b65bb
dharmesh-iyava <60869379+dharmesh-iyava@users.noreply.github.com>
2021-05-05 09:16:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from os import path
 import csv
 import psycopg2
 import datetime as dt
+import re
 
 app = Flask(__name__)
 
@@ -26,11 +27,13 @@ def createFolder(sourceFolder, folderName):
 def home():
     return '<h1 style="color:#007780;display: flex;justify-content: center;margin-top:250px;font-size:4rem;text-decoration:underline;">Welcome to Tenders2bidInfo</h1>'
 
-@app.route('/data/<string:mname>/<string:mphone>/<string:memail>/<string:mrevenue>/<string:name_1>/<string:position_1>/<string:email_1>/<string:phone_1>/<string:name_2>/<string:position_2>/<string:email_2>/<string:phone_2>/<string:name_3>/<string:position_3>/<string:email_3>/<string:phone_3>/<string:emp_name>')
+@app.route('/data/<string:mname>/<string:mphone>/<string:memail>/<string:mrevenue>/<string:name_1>/<string:position_1>/<string:email_1>/<string:phone_1>/<string:name_2>/<string:position_2>/<string:email_2>/<string:phone_2>/<string:name_3>/<string:position_3>/<string:email_3>/<string:phone_3>/<string:emp_name>/<string:maddress>')
 
-def sqldata(mname,mphone,memail,mrevenue,name_1,position_1,email_1,phone_1,name_2,position_2,email_2,phone_2,name_3,position_3,email_3,phone_3,emp_name):
+def sqldata(mname,mphone,memail,mrevenue,name_1,position_1,email_1,phone_1,name_2,position_2,email_2,phone_2,name_3,position_3,email_3,phone_3,emp_name,maddress):
     if request.method == 'GET': 
+        s1 = re.split(',', maddress)
+        maddress = s1[-1]
         cdate = dt.datetime.now().strftime('%Y-%m-%d %H:%M')
-        cursor.execute('INSERT INTO t2binfo(company_name, company_email, company_phone, company_revenue, te1_name, te1_position, te1_email, te1_phone, te2_name, te2_position, te2_email, te2_phone, te3_name, te3_position, te3_email, te3_phone, created_date, modified_by) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', (mname, memail, mphone, mrevenue, name_1, position_1, email_1, phone_1, name_2, position_2, email_2, phone_2, name_3, position_3, email_3, phone_3, cdate, emp_name))
+        cursor.execute('INSERT INTO t2binfo(company_name, company_email, company_phone, company_revenue, te1_name, te1_position, te1_email, te1_phone, te2_name, te2_position, te2_email, te2_phone, te3_name, te3_position, te3_email, te3_phone, created_date, modified_by, country) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', (mname, memail, mphone, mrevenue, name_1, position_1, email_1, phone_1, name_2, position_2, email_2, phone_2, name_3, position_3, email_3, phone_3, cdate, emp_name, maddress))
         conn.commit()
     return 'Data Transfered Successfully', 200
